c3cb6b512977f82b95392afdbf418dc76546e6bc
JacobF94 <jacob.frensley@gmail.com>
2021-11-18 15:15:48 -0600

    first commit

---
+++ note.js
@@ -0,0 +1,10 @@
+const notes = [
+    {
+        id: 1,
+        subject: "javascript for...of loops, arrays, and general JS variables",
+        date: "11/17/21",
+        feeling: "overwhelmed, but also good because at the end of the day I got the code to work correctly :^)",
+        timeSpent: 480
+    }
+]
+console.log(notes)
